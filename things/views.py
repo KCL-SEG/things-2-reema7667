1a29fd78f33120c633559660a11bbd2a01b5c6d2
Reema Alhamdan <reema.alhamdan@kcl.ac.uk>
2023-12-08 12:18:38 +0000

    fix

--- things/views.py
+++ things/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .froms import ThingForm
+from .forms import ThingForm
 def home(request):
     if request.method == 'POST':
         form = ThingForm(request.POST)
